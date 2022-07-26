14ab0864cdc9c55ee4616dbc61f14a0f094fbf46
Gogotsmod <96406101+Gogotsmod@users.noreply.github.com>
2022-07-26 10:14:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,32 +4,32 @@ global.mods = []
 global.prems = [] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Gempy⛧⁩⁩'
-global.numberowner = '6287898307350'
-global.instagram = 'https://www.instagram.com/gmpyyy'
-global.github = 'https://github.com/gmpyyy'
-global.dana = '087898307350'
-global.pulsa = '087898307350'
-global.gopay = '-'
+global.nameowner = 'Hangga⛧⁩⁩'
+global.numberowner = '6281223163244'
+global.instagram = 'https://www.instagram.com/hanggawjy'
+global.github = 'https://github.com/Gogotsmod'
+global.dana = '081223163244'
+global.pulsa = '081223163244'
+global.gopay = '081223163244'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'PENTABOT'
-global.gc = 'https://chat.whatsapp.com/ElBB5NrJS637TYqjJLHuk2'
-global.web = 'https://www.youtube.com/channel/UCZoVmApPxtLYgUWwBD4nbCw' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'GOGOTSBOT'
+global.gc = 'https://chat.whatsapp.com/DpDLbX0UvXS6EgEKyT02FU'
+global.web = 'https://vt.tiktok.com/ZSRFP1kXG/' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg' //ini juga ganti 
+global.fotonya1 = 'https://i.postimg.cc/SxLz5ShV/thumbnail.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://i.postimg.cc/SxLz5ShV/thumbnail.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = 'PENTABOT'
+global.wm = 'GOGOTS BOT'
 global.watermark = wm
-global.wm2 = '                     「PENTABOT⁩ 」'
-global.wm3 = 'Gempy⛧'
+global.wm2 = '                     「GOGOTS BOT⁩ 」'
+global.wm3 = 'Hangga⛧'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = 'https://i.postimg.cc/MZjW7hb6/20220707-200605.jpg'
+global.media = 'https://i.postimg.cc/SxLz5ShV/thumbnail.jpg'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wait = 'Process...'
 global.eror = 'ERROR'
